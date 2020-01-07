68354c1bad2a0f9ae876ac5189840c19acb63fb4
DaftBeowulf <ajmboosh@gmail.com>
2020-01-06 18:02:26 -0600

    :sparkles: Player now inits with the map currently in the map.txt

--- player.py
+++ player.py
@@ -18,11 +18,18 @@ class Player:
         self.status = []
         self.errors = []
         self.messages = []
+        self.map = self._read_map()
 
     def _get_status(self):
         r = requests.post(f"{url}/api/adv/status/",
                           headers={'Authorization': f"Token {key}", "Content-Type": "application/json"})
         return r.json()
+
+    def _read_map(self):
+        with open('map.txt') as map_file:
+            data = json.load(map_file)
+            print(f"current map: {data}")
+            return data
 
     def check_room(self):
         r = requests.get(f"{url}/api/adv/init/",
@@ -41,10 +48,10 @@ class Player:
         self.gold = data['gold']
         self.bodywear = data['bodywear']
         self.footwear = data['footwear']
-        self.inventory = []
-        self.status = []
-        self.errors = []
-        self.messages = []
+        self.inventory = data['inventory']
+        self.status = data['status']
+        self.errors = data['errors']
+        self.messages = data['messages']
 
     # def travel(self, direction, showRooms=False):
     #     nextRoom = self.currentRoom.getRoomInDirection(direction)
