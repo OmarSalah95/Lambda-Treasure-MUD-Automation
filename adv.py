4f3318a1c59737c9c218c97de33a96df2a66da46
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2020-01-07 12:31:47 -0800

    Small change forgot to save before last push

--- adv.py
+++ adv.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 import json
 
@@ -6,31 +5,6 @@ from player import Player
 from api import url, key
 
 player = Player()
+
 player.travel('n')
-# def add_to_map(room):
-#     map = read_map()
-#     if room['room_id'] in map:
-#         print("Room already in map")
-#     else:
-#         map = {}
-
-#         # map[data['room_id']] = data
-
-#         # with open('map.txt', 'w') as out:
-#         #     json.dump(map, out)
-
-
-# def move(direction):
-#     pass
-=======
-import requests
-import json
-
-from player import Player
-from api import url, key
-
-player = Player()
-
-# player.travel('n')
 # player.travel('s')
->>>>>>> 98e4cfda5f03fc186f8e0e0e09e3a1d7528ffe4d
